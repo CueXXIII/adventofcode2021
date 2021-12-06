27496f4dd2588c3d41eac0e747bc89f76ec933e5
CueXXIII <twitch.tv/cuexxiii>
2021-12-06 08:26:45 +0100

    day 6 part 2
    refactor and solve

--- 06 - Lanternfish/growth.cc
+++ 06 - Lanternfish/growth.cc
@@ -4,6 +4,23 @@
 #include <iostream>
 #include <string>
 
+void age(auto &lanternfish) {
+  const auto spawns = lanternfish[0];
+  for (auto i = 0; i < 8; ++i) {
+    lanternfish[i] = lanternfish[i + 1];
+  }
+  lanternfish[8] = spawns;
+  lanternfish[6] += spawns;
+}
+
+uint64_t count(auto &lanternfish) {
+  uint64_t sum = 0;
+  for (auto i = 0; i < 9; ++i) {
+    sum += lanternfish[i];
+  }
+  return sum;
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " <ages.txt>\n";
@@ -21,17 +38,22 @@ int main(int argc, char **argv) {
       lanternfish.at(c - '0') += 1;
     }
   }
-  for (auto n = 0; n < 80; n++) {
-    const auto spawns = lanternfish[0];
-    for (auto i = 0; i < 8; ++i) {
-      lanternfish[i] = lanternfish[i + 1];
-    }
-    lanternfish[8] = spawns;
-    lanternfish[6] += spawns;
+
+  for (auto n = 0; n < 18; n++) {
+    age(lanternfish);
   }
-  uint64_t sum = 0;
-  for (auto i = 0; i < 9; ++i) {
-    sum += lanternfish[i];
+  std::cout << "There are " << count(lanternfish)
+            << " lanternfish after 18 days.\n";
+
+  for (auto n = 18; n < 80; n++) {
+    age(lanternfish);
   }
-  std::cout << "There are " << sum << " lanternfish after 26 days.\n";
+  std::cout << "There are " << count(lanternfish)
+            << " lanternfish after 80 days.\n";
+
+  for (auto n = 80; n < 256; n++) {
+    age(lanternfish);
+  }
+  std::cout << "There are " << count(lanternfish)
+            << " lanternfish after 256 days.\n";
 }
