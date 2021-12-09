f72340358ae90a3b5f40feb0a76bb0eb17c4aa4a
CueXXIII <twitch.tv/cuexxiii>
2021-12-09 10:48:51 +0100

    day 9 part 2

--- 09 - Smoke Basin/flow.cc
+++ 09 - Smoke Basin/flow.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -18,10 +19,16 @@ private:
 
   std::vector<Vec2> lowPoints;
 
+  std::vector<uint8_t> filled;
+  std::vector<size_t> basinSizes;
+
 public:
   uint8_t &at(const size_t x, const size_t y) { return map[x + y * width]; }
-
   uint8_t &at(const Vec2 &pos) { return map[pos.x + pos.y * width]; }
+
+  uint8_t &fillPos(const size_t x, const size_t y) {
+    return filled[x + y * width];
+  }
 
 private:
   void findLowPoints() {
@@ -36,10 +43,47 @@ private:
     }
   }
 
+private:
+  size_t doFlood(const size_t x, const size_t y) {
+    if (at(x, y) == 9) {
+      return 0;
+    }
+    if (fillPos(x, y)) {
+      return 0;
+    }
+
+    fillPos(x, y) = true;
+    size_t fillCount = 1;
+    fillCount += doFlood(x - 1, y);
+    fillCount += doFlood(x + 1, y);
+    fillCount += doFlood(x, y - 1);
+    fillCount += doFlood(x, y + 1);
+    return fillCount;
+  }
+
+  void floodFill() {
+    filled.resize(width * height);
+    for (size_t y = 1; y < height - 1; ++y) {
+      for (size_t x = 1; x < width - 1; ++x) {
+        if (!fillPos(x, y)) {
+          const auto size = doFlood(x, y);
+          if (size > 0) {
+            basinSizes.push_back(size);
+          }
+        }
+      }
+    }
+  }
+
 public:
   size_t totalRiskLevel() {
     return std::accumulate(lowPoints.begin(), lowPoints.end(), 0,
                            [this](auto a, auto b) { return a + 1 + at(b); });
+  }
+
+  size_t largeBasinSizes() const {
+    return std::accumulate(basinSizes.end() - 3, basinSizes.end(), 1,
+                           [](auto a, auto b) { return a * b; });
   }
 
   friend std::ifstream &operator>>(std::ifstream &in, HeightMap &me) {
@@ -63,6 +107,10 @@ public:
     }
     me.height += 2;
     me.findLowPoints();
+    me.floodFill();
+    // partially sort basinSizes
+    std::nth_element(me.basinSizes.begin(), me.basinSizes.end() - 3,
+                     me.basinSizes.end());
     return in;
   }
 
@@ -74,6 +122,7 @@ public:
       out << '\n';
     }
     out << '\n';
+
     out << "LowPoints = {";
     bool first = true;
     for (const auto pos : me.lowPoints) {
@@ -84,6 +133,18 @@ public:
       first = false;
     }
     out << "}\n\n";
+
+    out << "Basins = {";
+    first = true;
+    for (const auto baSize : me.basinSizes) {
+      if (!first) {
+        out << ", ";
+      }
+      out << baSize;
+      first = false;
+    }
+    out << "}\n\n";
+
     return out;
   }
 };
@@ -102,4 +163,6 @@ int main(int argc, char **argv) {
   // std::cout << oceanFloor;
 
   std::cout << "Total risk level = " << oceanFloor.totalRiskLevel() << ".\n";
+  std::cout << "Product of the 3 largest basin sizes is "
+            << oceanFloor.largeBasinSizes() << ".\n";
 }
