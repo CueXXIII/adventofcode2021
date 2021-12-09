d8da9879cf22edc70181534c76dc02d00c8ccdb1
CueXXIII <twitch.tv/cuexxiii>
2021-12-09 08:43:00 +0100

    day 9 skeleton + inputs

---
+++ 09 - Smoke Basin/flow.cc
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <heightmap.txt>\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::ifstream infile{argv[1]};
+
+}
