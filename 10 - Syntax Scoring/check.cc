19e71b9d5812ade4a982002ea229664b6cdf0bcb
CueXXIII <twitch.tv/cuexxiii>
2021-12-10 07:20:27 +0100

    day 10 part 2

--- 10 - Syntax Scoring/check.cc
+++ 10 - Syntax Scoring/check.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
 
 enum Direction { open, close };
 
@@ -15,21 +16,21 @@ public:
 Bracket parseBracket(char c) {
   switch (c) {
   case '(':
-    return {0, open};
-  case ')':
-    return {0, close};
-  case '[':
     return {1, open};
-  case ']':
+  case ')':
     return {1, close};
-  case '{':
+  case '[':
     return {2, open};
-  case '}':
+  case ']':
     return {2, close};
-  case '<':
+  case '{':
     return {3, open};
-  case '>':
+  case '}':
     return {3, close};
+  case '<':
+    return {4, open};
+  case '>':
+    return {4, close};
   }
   // not reached, don't care
   return {0, open};
@@ -41,6 +42,8 @@ private:
 
   bool isCorrupt{false};
   char corruptToken{' '};
+
+  uint64_t autocomplete{0};
 
 public:
   uint64_t corruptScore() const {
@@ -58,6 +61,8 @@ public:
     }
     return 0;
   }
+
+  uint64_t autocompleteScore() const { return autocomplete; }
 
   friend std::ifstream &operator>>(std::ifstream &in, Line &line) {
     if (!(in >> line.source)) {
@@ -78,6 +83,13 @@ public:
         }
       }
     }
+    if (!line.isCorrupt) {
+      while (!stack.empty()) {
+        line.autocomplete *= 5;
+        line.autocomplete += stack.top().type;
+        stack.pop();
+      }
+    }
     return in;
   }
 };
@@ -89,13 +101,22 @@ int main(int argc, char **argv) {
   }
 
   std::ifstream infile{argv[1]};
-  uint64_t corruptScore{0};
+  uint64_t FIC{0};
+  std::vector<uint64_t> ACS{};
   for (;;) {
     Line line;
     if (!(infile >> line)) {
       break;
     }
-    corruptScore += line.corruptScore();
+    FIC += line.corruptScore();
+    const uint64_t lineACS = line.autocompleteScore();
+    if (lineACS > 0) {
+      ACS.push_back(lineACS);
+    }
   }
-  std::cout << "This syntax scores " << corruptScore << " points for FIC.\n";
+  const auto ACSmedian = ACS.begin() + ACS.size() / 2;
+  std::nth_element(ACS.begin(), ACSmedian, ACS.end());
+
+  std::cout << "This syntax scores " << FIC << " points for FIC.\n";
+  std::cout << "The overall ACS is " << *ACSmedian << " points.\n";
 }
