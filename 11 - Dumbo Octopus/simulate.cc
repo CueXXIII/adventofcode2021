bd119a560a82337e0727a41803c5ad1ff8d2316d
CueXXIII <twitch.tv/cuexxiii>
2021-12-11 15:02:09 +0100

    day 11: move data into class Cavern

--- 11 - Dumbo Octopus/simulate.cc
+++ 11 - Dumbo Octopus/simulate.cc
@@ -6,51 +6,68 @@
 
 #include "digit_field.hpp"
 
-size_t substep(auto &&field, auto &&flashing, const size_t width,
-               const size_t height) {
-  size_t flashSeen = 0;
-  for (size_t y = 1; y < height - 1; ++y) {
-    for (size_t x = 1; x < width - 1; ++x) {
-      if (!flashing[x + y * width] && field[x + y * width] > 9) {
-        flashSeen += 1;
-        flashing[x + y * width] = true;
-        for (const auto dx : {-1, 0, 1}) {
-          for (const auto dy : {-1, 0, 1}) {
-            field[(x + dx) + (y + dy) * width] += 1;
+class Cavern {
+private:
+  std::vector<uint8_t> field;
+  size_t width;
+  size_t height;
+
+private:
+  size_t pos(size_t x, size_t y) const { return x + y * width; }
+
+  size_t substep(auto &&flashing) {
+    size_t flashSeen = 0;
+    for (size_t y = 1; y < height - 1; ++y) {
+      for (size_t x = 1; x < width - 1; ++x) {
+        if (!flashing[pos(x, y)] && field[pos(x, y)] > 9) {
+          flashSeen += 1;
+          flashing[pos(x, y)] = true;
+          for (const auto dx : {-1, 0, 1}) {
+            for (const auto dy : {-1, 0, 1}) {
+              field[pos(x + dx, y + dy)] += 1;
+            }
           }
         }
       }
     }
+    return flashSeen;
   }
-  return flashSeen;
-}
 
-size_t step(auto &&field, const size_t width, const size_t height) {
-  size_t flashes, flashesTotal = 0;
-  for (auto &cell : field) {
-    cell += 1;
-  }
-  std::vector<bool> flashing;
-  flashing.resize(width * height, false);
-  while ((flashes = substep(field, flashing, width, height)) > 0) {
-    flashesTotal += flashes;
-  }
-  for (auto &cell : field) {
-    if (cell > 9) {
-      cell = 0;
+public:
+  size_t step() {
+    size_t flashes, flashesTotal = 0;
+    for (auto &cell : field) {
+      cell += 1;
     }
+    std::vector<bool> flashing;
+    flashing.resize(width * height, false);
+    while ((flashes = substep(flashing)) > 0) {
+      flashesTotal += flashes;
+    }
+    for (auto &cell : field) {
+      if (cell > 9) {
+        cell = 0;
+      }
+    }
+    return flashesTotal;
   }
-  return flashesTotal;
-}
 
-void printField(auto &&field, size_t width, size_t height) {
-  for (size_t y = 1; y < height - 1; ++y) {
-    for (size_t x = 1; x < width - 1; ++x) {
-      std::cout << static_cast<char>(field[x + y * width] + '0');
+  friend std::ostream &operator<<(std::ostream &out, Cavern &cavern) {
+    for (size_t y = 1; y < cavern.height - 1; ++y) {
+      for (size_t x = 1; x < cavern.width - 1; ++x) {
+        out << static_cast<char>(cavern.field[cavern.pos(x, y)] + '0');
+      }
+      out << "\n";
     }
-    std::cout << "\n";
+    return out;
   }
-}
+
+  friend std::ifstream &operator>>(std::ifstream &in, Cavern &cavern) {
+    std::tie(cavern.width, cavern.height) =
+        aoc::parseDigitField(in, cavern.field);
+    return in;
+  }
+};
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -58,14 +75,13 @@ int main(int argc, char **argv) {
     std::exit(EXIT_FAILURE);
   }
 
+  Cavern cavern;
   std::ifstream infile{argv[1]};
-  std::vector<uint8_t> field;
-  size_t width, height;
-  std::tie(width, height) = aoc::parseDigitField(infile, field);
+  infile >> cavern;
 
   size_t totalFlashes = 0;
   for (auto i = 0; i < 100; ++i) {
-    totalFlashes += step(field, width, height);
+    totalFlashes += cavern.step();
   }
   std::cout << "After 100 steps, you have seen " << totalFlashes
             << " flashes\n";
@@ -74,7 +90,7 @@ int main(int argc, char **argv) {
   size_t stepNo = 100;
   for (;;) {
     ++stepNo;
-    if (step(field, width, height) == 100) {
+    if (cavern.step() == 100) {
       std::cout << "In step " << stepNo
                 << " all 100 octopuses flash simultaneously.\n";
       break;
