990b63d6d7a90ca1291a3537f7a401807ed1353e
CueXXIII <twitch.tv/cuexxiii>
2021-12-14 11:24:00 +0100

    day 14 clean up the bit shift mess and more
    Still use the same bitshift, but hash as int instead of uint16_t, wich
    saves a cast. Cleaned up counting and output, too.

--- 14 - Extended Polymerization/reinforce.cc
+++ 14 - Extended Polymerization/reinforce.cc
@@ -2,40 +2,48 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <unordered_map>
-#include <vector>
+
+using charPair = std::array<char, 2>;
+
+template <> struct std::hash<charPair> {
+  std::size_t operator()(const charPair &word) const noexcept {
+    return std::hash<int>{}(word[0] << 8 | word[1]);
+  }
+};
+
+std::ostream &operator<<(std::ostream &out, const charPair &c) {
+  return out << c[0] << c[1];
+};
 
 class Rule {
 private:
-  const uint16_t input; // needed?
-  const std::array<const uint16_t, 2> output;
+  const std::array<const charPair, 2> output;
 
 public:
   Rule(const std::string &rule)
-      : input{static_cast<uint16_t>((rule[0] << 8) | (rule[1]))},
-        output{static_cast<uint16_t>((rule[0] << 8) | (rule[6])),
-               static_cast<uint16_t>((rule[6] << 8) | (rule[1]))} {}
+      : output{charPair{rule[0], rule[6]}, charPair{rule[6], rule[1]}} {}
 
   decltype(output) get() const { return output; }
 };
 
 class Template {
-  std::unordered_map<uint16_t, uint64_t> pairs;
+  std::unordered_map<charPair, uint64_t> pairs;
   char lastElement;
-  std::unordered_map<uint16_t, Rule> rules;
+  std::unordered_map<charPair, Rule> rules;
 
 public:
   Template(const std::string &polymer) {
     for (auto pos = size_t{0}; pos < polymer.size() - 1; ++pos) {
-      const uint16_t elementPair{
-          static_cast<uint16_t>((polymer[pos] << 8) | (polymer[pos + 1]))};
+      const charPair elementPair{polymer[pos], polymer[pos + 1]};
       ++pairs[elementPair];
     }
     lastElement = polymer[polymer.size() - 1];
   }
 
   void addRule(const std::string &rule) {
-    const uint16_t id{static_cast<uint16_t>((rule[0] << 8) | (rule[1]))};
+    const charPair id{rule[0], rule[1]};
     rules.emplace(id, rule);
   }
 
@@ -56,30 +64,29 @@ public:
   }
 
   friend std::ostream &operator<<(std::ostream &out, Template &t) {
-    std::array<uint64_t, 128> count;
-    count.fill(0);
+    std::map<char, uint64_t> count;
     for (const auto &pair : t.pairs) {
-      count[pair.first >> 8] += pair.second;
+      count[pair.first[0]] += pair.second;
     }
     count[t.lastElement]++;
 
-    out << "{";
+    out << "ElementCount = {";
     bool first = true;
     uint64_t min{0}, max{0};
-    for (auto element = 'A'; element <= 'Z'; ++element) {
-      if (count[element] > 0) {
+    for (const auto &element : count) {
+      if (element.second > 0) {
         if (!first) {
           out << ", ";
         } else {
-          min = max = count[element];
+          min = max = element.second;
           first = false;
         }
-        out << element << ":" << count[element];
-        min = std::min(min, count[element]);
-        max = std::max(max, count[element]);
+        out << element.first << ":" << element.second;
+        min = std::min(min, element.second);
+        max = std::max(max, element.second);
       }
     }
-    out << "} q=" << max - min;
+    out << "}\nQuantitiy value = " << max - min;
     return out;
   }
 };
@@ -99,13 +106,18 @@ int main(int argc, char **argv) {
     t.addRule(line);
   }
 
-  std::cout << t << "\n";
+  std::cout << "Initial polymer:\n";
+  std::cout << t << "\n\n";
+
   for (auto i = size_t{0}; i < 10; ++i) {
     t.step();
   }
-  std::cout << t << "\n";
+  std::cout << "After 10 insertion steps:\n";
+  std::cout << t << "\n\n";
+
   for (auto i = size_t{10}; i < 40; ++i) {
     t.step();
   }
+  std::cout << "After 40 insertion steps:\n";
   std::cout << t << "\n";
 }
