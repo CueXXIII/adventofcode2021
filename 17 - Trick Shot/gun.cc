b30b422e2a1bec58085a7429bacc4e40174a243b
CueXXIII <twitch.tv/cuexxiii>
2021-12-17 07:04:11 +0100

    day 17 part 1

--- 17 - Trick Shot/gun.cc
+++ 17 - Trick Shot/gun.cc
@@ -1,6 +1,14 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <regex>
+
+struct Area {
+  int64_t minX;
+  int64_t maxX;
+  int64_t minY;
+  int64_t maxY;
+};
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -9,5 +17,21 @@ int main(int argc, char **argv) {
   }
 
   std::ifstream infile{argv[1]};
+  std::string line;
+  std::getline(infile, line);
 
+  const std::regex matchTargetArea{
+      "target area: x=(-?\\d+)\\.\\.(-?\\d+), y=(-?\\d+)\\.\\.(-?\\d+)"};
+  std::smatch matchedCoords;
+  std::regex_match(line, matchedCoords, matchTargetArea);
+
+  const Area targetArea{
+      std::stol(matchedCoords[1]), std::stol(matchedCoords[2]),
+      std::stol(matchedCoords[3]), std::stol(matchedCoords[4])};
+
+  // to hit bottom of targetArea
+  const int64_t maxXVel = -targetArea.minY - 1;
+
+  std::cout << "The maximum height to reach the area in style is "
+            << maxXVel * (maxXVel + 1) / 2 << "\n";
 }
