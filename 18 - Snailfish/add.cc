e55b9c29502c64513310c4d993759df8af229311
CueXXIII <twitch.tv/cuexxiii>
2021-12-18 06:00:57 +0100

    day 18 skeleton

---
+++ 18 - Snailfish/add.cc
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <homework.txt>\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::ifstream infile{argv[1]};
+
+}
