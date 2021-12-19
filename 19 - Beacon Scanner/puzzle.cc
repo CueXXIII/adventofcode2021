5ece33b59f10f475ffa581694c55a05b4c2bbc2e
CueXXIII <twitch.tv/cuexxiii>
2021-12-19 06:25:56 +0100

    day 19 skeleton + inputs

---
+++ 19 - Beacon Scanner/puzzle.cc
@@ -0,0 +1,11 @@
+#include "all.hpp"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <scan.txt>\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::ifstream infile{argv[1]};
+
+}
